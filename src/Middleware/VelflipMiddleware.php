0e8e9ac671d159a75b198bf6781e20979282a24a
Endru Reza <lotusb13@gmail.com>
2018-08-29 13:35:29 +0700

    Prewrite code

---
+++ src/Middleware/VelflipMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the endru/velflip package.
+ *
+ * (c) Endru Reza <lotusb13@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/endrureza/velflip
+ */
+
+namespace Endru\Velflip\Middleware;
+
+class VelflipMiddleware
+{
+    
+}
